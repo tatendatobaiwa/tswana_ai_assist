058f4373859934cf1791ebbcaec613362c3db5fc
tatendatobaiwa <tatenda.tobaiwa@gmail.com>
2025-06-09 20:06:44 +0200

    lgtm: init

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -1,15 +1,16 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore'; // For Firestore
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyCiG_cDjrxU4VuWW03B757hCLR1GedO4KM",
+  authDomain: "tswanapp.firebaseapp.com",
+  projectId: "tswanapp",
+  storageBucket: "tswanapp.firebasestorage.app",
+  messagingSenderId: "14459649912",
+  appId: "1:14459649912:web:667db4504677cd7e72d28d",
+  measurementId: "G-C7E8RDHBDC"
 };
 
 // Initialize Firebase
@@ -20,5 +21,6 @@ export const firebaseAuth = getAuth(app);
 
 // Initialize Cloud Firestore and get a reference to the service
 export const db = getFirestore(app);
+const analytics = getAnalytics(app);
 
 export default app;
